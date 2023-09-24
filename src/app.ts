a381bab38f3ecc57ef0b1f9b0e54507821ab4ccc
Mikael <mikaelmuniz2001@gmail.com>
2023-09-24 11:27:34 -0300

    change cors

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 require('dotenv').config();
 
 app.use(logger('dev'));
-app.use(cors({origin: 'https://webscraper-front-vied.vercel.app'}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', router);
