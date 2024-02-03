37be9fd4f3700e3d0c429e6c7817c867efed21fc
Mikael <mikaelmuniz2001@gmail.com>
2024-02-03 17:15:55 -0300

    fix: change https to http

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,10 @@
-import https from 'https';
+import http from 'https';
 import app from './app';
 
-const fs = require('fs');
-
-let server = https.createServer({
-    cert: fs.readFileSync('src/SSL/code.crt'),
-    key: fs.readFileSync('src/SSL/code.key'),
-}, app);
+let server = http.createServer(app);
 
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
     return console.log(`Port listening at port ${port}`);
-})
+});
